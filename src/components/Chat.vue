a4b1d1faddc2af3b7ed2080ad95ddab1a0ab7416
markgmz92 <markgmz92@gmail.com>
2020-05-11 19:19:09 +0800

    Added colon to line 15

--- src/components/Chat.vue
+++ src/components/Chat.vue
@@ -12,7 +12,7 @@
                 </ul>
             </div>
             <div class="card-action">
-                <NewMessage name="name" />
+                <NewMessage :name="name" />
             </div>
         </div>
     </div>
